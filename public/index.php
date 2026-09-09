b902137dc54c79dc4cab762b044177de7e435e0d
Ashly Naomi Sanchez <174140202+Anst1919@users.noreply.github.com>
2026-09-09 09:11:50 -0600

    cambio nao miercoles jeje

--- public/index.php
+++ public/index.php
@@ -9,10 +9,9 @@
     <h1>hola amiguitos jejej</h1>
     <h2></h2>hola soy Ashly jejej</h2>
     <h3>hola a todos jejej</h3>
-    </h4>hola soy valeria si hola</h4>
+    <h4>hola soy valeria si hola</h4>
     <h5>pcam2</h5>
     <h6>CFE ultimo cambio</h6>
-    <h7>so</h7>
-    <h8>rra</h8>
+
 </body>
 </html>
